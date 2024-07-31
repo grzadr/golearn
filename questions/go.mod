1a3563a991c412da80f70f1330cdc8d5fa448f58
Adrian Grzemski <adrian.grzemski@gmail.com>
2024-07-31 14:04:50 +0200

    initial commit

---
+++ questions/go.mod
@@ -0,0 +1,3 @@
+module github.com/grzadr/questions
+
+go 1.22.5
---
+++ questions/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/grzadr/questions/question"
+)
+
+func main() {
+	questionFile := filepath.Join("data", "questions.json")
+	questions, err := question.LoadQuestions(questionFile)
+	if err != nil {
+		log.Fatal("Error loading questions:", err)
+	}
+
+	fmt.Println("Quiz loaded successfully!")
+	fmt.Printf("Number of questions: %d\n", len(questions))
+
+	// We'll add the quiz logic here in the next step
+}
